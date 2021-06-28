ebc91ebf5edb5f4198a8dc8c30ab0dcb042ceb5b
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-06-28 19:41:46 +0530

    changed Ts mode and conditionally renderquick view buttons

--- stash-get/src/app/shared/product/product-card/product-card.component.ts
+++ stash-get/src/app/shared/product/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -9,12 +9,29 @@ export class ProductCardComponent implements OnInit {
 
   productUrl = 'https://www.mountaingoatsoftware.com/uploads/blog/2016-09-06-what-is-a-product.png';
 
-  @ViewChild('quickView') quickViewButton: any;
   showButton = false;
+
+  @HostListener('mouseenter') mouseOver(evDate: Event) {
+    this.showButton = true;
+  }
+
+  @HostListener('mouseleave') mouseLeave(evDate: Event) {
+    this.showButton = false;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showQuickButton() {
+    console.log('Something open')
+    this.showButton = true;
+  }
+
+  hideQuickButton() {
+    console.log('Something closed')
+    this.showButton = false;
   }
 
 }
