3f682264aa8f5e96917d940173d89bf5aa3e6ee0
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-06-29 17:29:52 +0530

    Added carousel on home page and dependency for jquery

--- stash-get/src/app/home-page/home-page.component.ts
+++ stash-get/src/app/home-page/home-page.component.ts
@@ -9,7 +9,7 @@ export class HomePageComponent implements OnInit {
 
   featuredProdUrl_1 = 'https://i.pinimg.com/originals/c2/69/fe/c269fe456686bb9f0395c97ba2b2968c.jpg';
   featuredProdUrl_2 = 'https://i.pinimg.com/originals/59/bd/e9/59bde957a1f936ecdd99a0da6d5d3c1c.jpg';
-  featuredProdUrl_3 = 'https://believers4ever.com/wp-content/uploads/2019/04/Do-not-worry-Facebook-timeline-cover.jpg';
+  featuredProdUrl_3 = 'https://i.pinimg.com/originals/87/65/3f/87653f5bce37ae8e7864613e68479be8.jpg';
 
   constructor() { }
 
