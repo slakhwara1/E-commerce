2e004ecf01f85caf79c086443a4b2a88f38a97ec
slakhwara1 <30632172+slakhwara1@users.noreply.github.com>
2021-06-29 18:01:08 +0530

    Changed data for url for carousel image

--- stash-get/src/app/home-page/home-page.component.ts
+++ stash-get/src/app/home-page/home-page.component.ts
@@ -9,7 +9,7 @@ export class HomePageComponent implements OnInit {
 
   featuredProdUrl_1 = 'https://i.pinimg.com/originals/c2/69/fe/c269fe456686bb9f0395c97ba2b2968c.jpg';
   featuredProdUrl_2 = 'https://i.pinimg.com/originals/59/bd/e9/59bde957a1f936ecdd99a0da6d5d3c1c.jpg';
-  featuredProdUrl_3 = 'https://i.pinimg.com/originals/87/65/3f/87653f5bce37ae8e7864613e68479be8.jpg';
+  featuredProdUrl_3 = 'https://i.pinimg.com/originals/59/24/09/592409482b987b9524248f60bc12abaa.jpg';
 
   constructor() { }
 
